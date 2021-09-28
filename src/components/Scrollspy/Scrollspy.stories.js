b0a97e6aa73b4c4dab636dea7922bec7a00ce71d
nikhil-isaac <37073895+nikhil-isaac@users.noreply.github.com>
2021-09-29 04:17:13 +0530

    adding scrollspy story

---
+++ src/components/Scrollspy/Scrollspy.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Sample } from "./Sample";
+
+export default {
+  title: "Components/Scrollspy",
+  component: Sample,
+};
+
+export const Example = () => (
+  <>
+    <Sample />
+  </>
+);
